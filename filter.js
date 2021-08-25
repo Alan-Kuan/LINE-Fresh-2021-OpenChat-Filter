a2933c694bee54166d1124a8237c32e6f85dc7cb
Alan-Kuan <kuanhaoan@gmail.com>
2021-08-25 11:03:40 +0800

    Wrap url links with anchor tags.

--- filter.js
+++ filter.js
@@ -38,7 +38,7 @@ line_reader.on('line', function (line) {
     // next line of previous message
     } else {
         let len = history[current_date].length;
-        history[current_date][len - 1].content.push(line);
+        history[current_date][len - 1].content.push(line.wrapURLs(true));
     }
 
     fs.writeFileSync('./filtered_messages.json', JSON.stringify(history));
@@ -49,6 +49,18 @@ function parseMessage(line) {
     return {
         time: regex[1],
         author: regex[2],
-        content: [ regex[3] ]
+        content: [ regex[3].wrapURLs(true) ]
     };
 }
+
+// Source: https://gist.github.com/ryansmith94/0fb9f6042c1e0af0d74f
+String.prototype.wrapURLs = function (new_window) {
+    var url_pattern = /(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}\-\x{ffff}]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?/ig;
+    var target = (new_window === true || new_window == null) ? '_blank' : '';
+
+    return this.replace(url_pattern, function (url) {
+        var protocol_pattern = /^(?:(?:https?|ftp):\/\/)/i;
+        var href = protocol_pattern.test(url) ? url : 'http://' + url;
+        return '<a href="' + href + '" target="' + target + '">' + url + '</a>';
+    });
+};
